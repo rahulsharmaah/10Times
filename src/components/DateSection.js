72d8ff2129608ca3c470b14146178f124713e05f
Rahul Sharma <8470314+rahulsharmaah@users.noreply.github.com>
2023-11-30 05:56:55 +0530

    changes published for some icons

--- src/components/DateSection.js
+++ src/components/DateSection.js
@@ -41,7 +41,7 @@ const DateSection = () => {
       </div>
 
       {/* Start Date and End Date */}
-      <div className="flex flex-col items-center mt-2 border m-2 px-4 py-4 rounded-md bg-gray-50">
+      <div className="flex flex-col mt-2 border m-2 px-4 py-4 rounded-md bg-gray-50">
         <div className="flex items-center mb-2 gap-1">
           <div className="text-lg flex-1 font-medium text-gray-800">Start</div>
 
@@ -67,7 +67,7 @@ const DateSection = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-1">
+        <div className="flex items-center gap-1 ">
           <div className="flex-1 text-lg font-medium text-gray-800">End</div>
 
           {/* Date Picker Container */}
@@ -96,6 +96,46 @@ const DateSection = () => {
         {validationError && (
           <div className="text-red-500 mt-2">{validationError}</div>
         )}
+        <div className="flex items-center gap-3 py-3 border-b-2">
+          <div className="h-6 w-6 rounded-full font-medium text-slate-800 text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418"
+              />
+            </svg>
+          </div>
+          <div className="pl-3 text-gray-500 font-medium">
+            GMT +5:30 Kolkata
+          </div>
+        </div>
+        <div className="flex items-center gap-3 py-3 border-b-2">
+          <div className="h-6 w-6 rounded-full font-medium text-slate-800 text-gray-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.429 9.75L2.25 12l4.179 2.25m0-4.5l5.571 3 5.571-3m-11.142 0L2.25 7.5 12 2.25l9.75 5.25-4.179 2.25m0 0L21.75 12l-4.179 2.25m0 0l4.179 2.25L12 21.75 2.25 16.5l4.179-2.25m11.142 0l-5.571 3-5.571-3"
+              />
+            </svg>
+          </div>
+          <div className="pl-3 text-gray-500 font-medium">
+            Create Multi-Session Event
+          </div>
+        </div>
       </div>
     </div>
   );
