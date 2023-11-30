8a7ea543cb334b1a313debda51212c04a7048971
Rahul Sharma <8470314+rahulsharmaah@users.noreply.github.com>
2023-11-30 11:43:07 +0530

    changes to footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer class="bg-gray-50">
+    <footer class="bg-teal-100">
     <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
       <div class="sm:flex sm:items-center sm:justify-between">
-        <div class="flex justify-center text-4xl font-medium text-teal-600 sm:justify-start">
+        <div class="flex justify-center text-4xl font-medium text-slate-700 sm:justify-start">
           Events Creator
         </div>
-        <p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+        <p class="mt-4 text-center font-medium text-sm text-slate-800 lg:mt-0 lg:text-right">
           Copyright &copy; 2023. All rights reserved.
         </p>
       </div>
--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const PageHeader = () => {
   return (
-    <header className="bg-gray-50">
+    <header className="bg-teal-100">
       <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <div className="text-center sm:text-left">
